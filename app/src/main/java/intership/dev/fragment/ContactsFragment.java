e1868de6ba702bd4e0b9c1bae57981387ebd8b18
hoangthuan <alexcbtnthuan@gmail.com>
2015-07-21 17:21:29 +0700

    update file contactsFragment . add funtion load more in listview

--- app/src/main/java/intership/dev/fragment/ContactsFragment.java
+++ app/src/main/java/intership/dev/fragment/ContactsFragment.java
@@ -1,12 +1,15 @@
 package intership.dev.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
@@ -19,37 +22,105 @@ import intership.dev.pageadapter.ContactsPageAdapter;
  */
 public class ContactsFragment extends Fragment {
     private ListView lvContacts;
-    private ArrayList <ContactItem> mContacts;
     private ContactsPageAdapter mContactsPageAdapter;
+    private ArrayList <ContactItem> mContacts;
+    private ProgressBar progressBar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contacts,container,false);
         init(view);
-        getData();
-        display();
+        new  getData().execute();
         return view;
     }
 
 
     public void init(View view){
         lvContacts = (ListView) view.findViewById(R.id.lvContacts);
-        mContacts = new ArrayList<ContactItem>();
+        progressBar = (ProgressBar)view.findViewById(R.id.progressBar);
+        progressBar.setVisibility(getView().VISIBLE);
 
     }
 
-    public  void getData(){
-        for(int i=0;i<5;i++){
-            mContacts.add(new ContactItem("Hoang thuan","noi em den xa lam",R.mipmap.img_contact1));
-            mContacts.add(new ContactItem("Vui Vui","noi em den xa lam",R.mipmap.img_contact2));
+
+
+    private class getData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
         }
 
-    }
-    public  void display(){
-        mContactsPageAdapter = new ContactsPageAdapter(getActivity(),mContacts);
-        lvContacts.setAdapter(mContactsPageAdapter);
-    }
+        @Override
+        protected Void doInBackground(Void... params) {
+               mContacts = new ArrayList<ContactItem>();;
+            for(int i=0;i<5;i++){
+                mContacts.add(new ContactItem("Hoang thuan","noi em den xa lam",R.mipmap.img_contact1));
+                mContacts.add(new ContactItem("Vui Vui","noi em den xa lam",R.mipmap.img_contact2));
+            }
+            return null;
+        }
 
+        @Override
+        protected void onPostExecute(Void result) {
+            mContactsPageAdapter = new ContactsPageAdapter(getActivity(),mContacts);
+            lvContacts.setAdapter(mContactsPageAdapter);
+            lvContacts.setOnScrollListener(new AbsListView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(AbsListView absListView, int i) {
+                    int count=lvContacts.getCount();
+
+                    if(i==SCROLL_STATE_IDLE){
+                        if(lvContacts.getLastVisiblePosition()>=count-1){
+                            new LoadMoreDataTask().execute();
+                        }
+
+                    }
+
+                }
+
+                @Override
+                public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+                }
+            });
+
+        }
+    }
+    private class LoadMoreDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(getView().VISIBLE);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if(mContacts.size()<40) {
+                for (int i = 0; i < 5; i++) {
+                    mContacts.add(new ContactItem("Hoang thuan", "noi em den xa lam", R.mipmap.img_contact1));
+                    mContacts.add(new ContactItem("Vui Vui", "noi em den xa lam", R.mipmap.img_contact2));
+                }
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            int position = lvContacts.getLastVisiblePosition();
+            mContactsPageAdapter = new ContactsPageAdapter(getActivity(),mContacts);
+            lvContacts.setAdapter(mContactsPageAdapter);
+
+            lvContacts.setSelectionFromTop(position, 0);
+            progressBar.setVisibility(getView().GONE);
+        }
+    }
 
 }
+
+
+
