b1e25a59bf8ac08bee512dda8b6d33ddd4bd5a47
hoangthuan <alexcbtnthuan@gmail.com>
2015-07-23 16:50:29 +0700

    update file contact Fragment , update funtion display contact

--- app/src/main/java/intership/dev/fragment/ContactFragment.java
+++ app/src/main/java/intership/dev/fragment/ContactFragment.java
@@ -1,26 +1,38 @@
 package intership.dev.fragment;
-
-import android.app.Fragment;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import intership.dev.contact.MainActivity;
 import intership.dev.contact.R;
 import intership.dev.customeview.CustomeCycleImageView;
+import intership.dev.item.ContactItem;
+import intership.dev.sqlite.ContactSqlite;
 
 /**
  * Created by hoangthuan on 7/22/2015.
  */
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements View.OnClickListener{
     private CustomeCycleImageView customeCycleImageView;
     private TextView tvNameContact;
     private EditText edtNameContact;
     private EditText edtDescription;
+    private ContactSqlite mContactSqlite;
+    private LinearLayout llCannel;
+    private LinearLayout llSave;
+    private SQLiteDatabase mDb;
+    private ContactItem mContactItem;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,17 +42,51 @@ public class ContactFragment extends Fragment {
         return  view;
     }
 
+    /**
+     * get id for component from file xml
+     * @param view
+     */
+
     public void init(View view){
+        mContactItem = (ContactItem)getArguments().getSerializable("SEND_DATA");
+        mContactSqlite = new ContactSqlite(getActivity());
         customeCycleImageView = (CustomeCycleImageView)view.findViewById(R.id.custom_ImageView);
         tvNameContact = (TextView)view.findViewById(R.id.tvNameContact);
         edtDescription = (EditText)view.findViewById(R.id.edtDeseription);
         edtNameContact = (EditText)view.findViewById(R.id.edtNameContact);
+        llCannel = (LinearLayout)view.findViewById(R.id.llCannel);
+        llCannel.setOnClickListener(this);
+        llSave = (LinearLayout)view.findViewById(R.id.llSave);
+        llSave.setOnClickListener(this);
     }
 
+    /**
+     * display data into layout
+     */
     public  void display(){
-        customeCycleImageView.setImageResource(MainActivity.CONTAC_ITEM.getmAvata());
-        tvNameContact.setText(MainActivity.CONTAC_ITEM.getmUsername());
-        edtNameContact.setText(MainActivity.CONTAC_ITEM.getmUsername());
-        edtDescription.setText(MainActivity.CONTAC_ITEM.getmDecreption());
+        customeCycleImageView.setImageResource(mContactItem.getmAvata());
+        tvNameContact.setText(mContactItem.getmUsername());
+        edtNameContact.setText(mContactItem.getmUsername());
+        edtDescription.setText(mContactItem.getmDecreption());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == llCannel){
+            Intent intent = new Intent(getActivity(),MainActivity.class);
+            startActivity(intent);
+        }
+        if(view == llSave){
+         mDb = mContactSqlite.getWritableDatabase();
+            if(mContactSqlite.updateContact(mContactItem.getmId(),edtNameContact.getText().toString(),edtDescription.getText().toString())){
+                Toast.makeText(getActivity(),R.string.messages_noti_update_success,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(getActivity(),R.string.messages_noti_update_error,Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
